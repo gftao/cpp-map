6d641c108cc7b69d9c0e5d7a02b34dca6d61fc1d
Guft <gufengtao@gmail.com>
2016-10-16 11:44:44 +0800

    map 3中insert

--- map/map.cpp
+++ map/map.cpp
@@ -11,6 +11,13 @@ int main()
 	mapStudents.insert(pair<int,string>(2,"student_two"));
 	mapStudents.insert(pair<int,string>(3,"student_three"));
 
+	mapStudents.insert(map<int,string>::value_type(4,"student_four"));
+	mapStudents.insert(map<int,string>::value_type(5, "student_five"));
+	mapStudents.insert(map<int,string>::value_type(6, "student_six"));
+
+	mapStudents[7] = "student_seven";
+	mapStudents[8] = "student_eight";
+
 	map<int,string>::iterator iter;
 	for (iter = mapStudents.begin(); iter!=mapStudents.end(); iter++)
 	{
